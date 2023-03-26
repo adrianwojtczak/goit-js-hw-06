dc3766cb78d70f368099e8991b1ca4d383414ea7
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-03-26 21:38:41 +0200

    update extra task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
